7f3ccc0d7467e750eb4fd03102e265b98ec9c5ee
Brendan Goodenough <brendan@goodenough.nz>
2016-08-14 23:43:10 +1200

    Display recent earthquakes with nice styling
    Filtering the earthquakes based on time (past hour, past day, past
    month) is still a work in progress. Date and location string
    structures may change to be more user friendly in the future.

--- app/models/earthquake.ts
+++ app/models/earthquake.ts
@@ -1,9 +1,11 @@
 export class Earthquake {
-  title: string;
+  country: string;
   place: string;
+  date: string;
   latitude: number;
   longitude: number;
   depth: number;
   magnitude: number;
+  intensity: number;
   time: number;
 }
--- app/pages/recent/recent.service.ts
+++ app/pages/recent/recent.service.ts
@@ -24,15 +24,39 @@ export class RecentService {
 
     for (let i = 0; i < features.length; i++) {
       let feature = features[i];
+
+      // Split the location based on country and general area
+      let place = feature.properties.place;
+      let location = place.substring(0, place.lastIndexOf(','));
+      let country = place.substring(place.lastIndexOf(',') + 1, place.length);
+
+      // Convert the date to a nicely formatted string.
+      // Example Format: 14/8/2016 @ 10:23 UTC
+      //
+      // TODO: Rewrite format to be "14 August, 2016 @ 10:23am UTC" to deal with
+      // the confusion America may face when presented with a logical date
+      // structure.
+      let time = feature.properties.time;
+      let date = new Date(time);
+      let dateString = date.getUTCDate() + '/' + (date.getUTCMonth() + 1) + '/'
+        + date.getUTCFullYear() + ' @ ' + date.getUTCHours() + ':'
+        + date.getUTCMinutes() + ' UTC';
+
+      // Create earthquake object
       let earthquake = new Earthquake();
 
-      // TODO: String title and place to hold different information
-      earthquake.title = feature.properties.place;
-      earthquake.place = feature.properties.place;
-      earthquake.magnitude = feature.properties.mag;
-      earthquake.latitude = feature.geometry.coordinates[0];
-      earthquake.longitude = feature.geometry.coordinates[1];
-      earthquake.depth = feature.geometry.coordinates[2];
+      earthquake.longitude = feature.geometry.coordinates[0];
+      earthquake.latitude = feature.geometry.coordinates[1];
+      earthquake.depth = feature.geometry.coordinates[2].toFixed(1);
+
+      earthquake.country = country;
+      earthquake.place = location;
+
+      earthquake.magnitude = feature.properties.mag.toFixed(1);
+      earthquake.intensity = feature.properties.mmi;
+
+      earthquake.time = time;
+      earthquake.date = dateString;
 
       earthquakes.push(earthquake);
     }
